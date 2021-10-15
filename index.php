b649253ee5d4072d0178842c9f3248f324c69cf3
xyrelljoi <xyrelljoi@gmail.com>
2021-10-15 10:04:28 +0800

    refractor: change and add some lines to improve the connection between the database and the site

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
-include_once("connection/connection.php");
+/*This is for admin and test - copy
+eto na gagamitin dzai*/
 
+include_once("connection/connection.php");
 $con = connection();
+
 /*TEST 1: cif the database is connected. RESULTS: true
 if($con->connect_error){
     echo $con->connect_error;
@@ -11,15 +14,14 @@ else{
     echo "Connected";
 }*/
 
-$sql = "SELECT * FROM student_list";
+$sql = "SELECT * FROM student_list ORDER BY id ASC";
 $students = $con ->query($sql) or die($con->error);
 $row = $students->fetch_assoc();
 
-/* TEST 2: IF THE DATABASE ARE DISPLAYING RESULTS: TRUE
- do{
+/* TEST 2: IF THE DATABASE ARE DISPLAYING RESULTS: TRUEdo{
     
     echo $row['first_name'];
-}while($row = $students->fetch_assoc());
+}while($row = $students->fetch_assoc()); 
 */
 
 if(isset($_POST['submit'])){
@@ -30,132 +32,124 @@ if(isset($_POST['submit'])){
    $sql = "INSERT INTO `student_list`(`first_name`, `email`) VALUES ('$fname','$mail')";
    $con->query($sql) or die ($con->error);
 
+   echo header("Location:index.php");
 }
 ?>
 <!--start of html-->
-<!DOCTYPE html>
-   <html>
-       <head>
+<!DOCTYPE htmL>
+<html lang ="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>DFCAMCLP-IT</title>
         <link rel ="stylesheet" href="css/style.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/0c9ae9856a.js" crossorigin="anonymous"></script>
-
         <!--CSS and JS links -->
         <link rel= "stylesheet" href = "css/style.css">
-        </head>
-       <body>
         
-        <!--BASIC INFO-->
-        <div class="bot-chat">
+    </head>
+    <body>
+
+            <!--inquirers-->
+        <div class="login">
             <input type="checkbox" id="show">
-            <label for="show" class="show-btn fas fa-comment-alt">
-            </label>
-            <div class ="wrapper">
-                <div class="header-title">
-                    <p>DFCAMCLP-IT ChatBot
+                <label for="show" class="show-btn fas fa-comment-alt">
+                </label>
+            <div class="container">
+                <div class="header">
+                    <h3>DFCAMCLP-IT CHATBOT
                         <label for="show" class="minimize-btn">
                             <i class="fas fa-times"></i>
                         </label>
-                    </p>
+                    </h3>
                 </div>
-
-                <div class="form">
-                    <p>Before anything else... Can we get your information so we may email the answers in case we are not able to respond immediately? By filling out this form, you are agreeing to DFCAMCLP-IT's
+                <form id="form1" action="" method="post">
+                    <p>Before anything else... Can we get your information so we may email the answers in case we are not able to respond immediately? By filling out this form, you are agreeing to Edukasyon's
                         <a href="#">Terms of Service</a>
-                        and 
-                        <a href="#">Privacy Policy</a>
-                        .
+                         and 
+                         <a href="#">Privacy Policy</a>
+                         .
                     </p>
-                </div>
+                    <label for ="firstname">First Name</label>
+                    <input type="textarea" name ="firstname"id="firstname">
 
-                <form class ="input-form" id="inquirerForm" action="" method="post">
-                    <ul>
-                        <li class = "form-field">
-                            <label for="firstname">First Name<span>*</span></label>
-                            <input type="text" name="firstname" id="firstname"required>
-                        </li>
-                    
-                        <li class = "form-field">
-                            <label for="email">Email<span>*</span></label>
-                            <input type="email" name="email" id="email"required>
-                        </li>
-                            <a href="#"id="btnStudent">Student of DFCAMCLP-IT? Click here</a>
-                        </li>
-                    </ul>
-                    <input type="submit" name ="submit" class="bot-btn" value="I accept and start chat">
+                    <label for ="email">email</label>
+                    <input type="email" name ="email" id="email">
+                    <input type="button" id="intoStudentForm" value="I am a student of DFCAMCLP-IT">
+                    <div id="btn-submit">
+                        <input class="button" type="submit" name ="submit" value="I accept and start chat">
+                    </div>
                 </form>
-
-                <form class ="input-form" id="studentForm">
-                    <ul>
-                        <li class = "form-field">
-                            <label for="studentId">Student Id<span>*</span></label>
-                        
-                            <input type="text" id="studentId"required>
-                        </li>
-                    </ul>
-                    
+                <!--students-->
+                <form id ="form2" action="" method="post">
+                    <p>Before anything else... Can we get your information so we may email the answers in case we are not able to respond immediately? By filling out this form, you are agreeing to Edukasyon's
+                        <a href="#">Terms of Service</a>
+                         and 
+                         <a href="#">Privacy Policy</a>
+                         .
+                    </p>
+                    <label for ="studentid">Student ID</label>
+                    <input type="textarea" name ="studentid"id="studentid">
+                    <div id="btn-submit2">
+                        <input class="button" type="submit" name ="submit" value="I accept and start chat">
+                    </div>
                 </form>
-                <!--<a href ="#" class="bot-btn">I accept and Start Chat</a>-->
-                <!--<input type="submit" name ="submit" class="bot-btn" value="I accept and start chat">-->
-                <!--<button type="submit" name="submit" class="bot-btn">I accept and start chat</button>-->
-                
             </div>
-        
         </div>
-        <!--<div class ="wrapper">
+            <script type="text/javascript">
+                var Form1 = document.getElementById("form1");
+                var Form2 = document.getElementById("form2");
+
+                var studentBtn = document.getElementById("intoStudentForm");
+
+                studentBtn.onclick = function(){
+                    Form1.style.left="-450px";
+                    Form2.style.left="20px";
+                }
+        </script>
+
+        <!--BOTCHAT
+        <div class="wrapper">
             <div class="header-title">DFCAMCLP-IT ChatBot</div>
-            <div class ="form">
-                <div class="bot-inbox inbox">   
-                    <div class="icon">
-                        <i class ="user"></i>
+                <div class ="form">
+                    <div class="bot-inbox inbox">   
+                        <div class="icon">
+                            <i class ="user"></i>
+                        </div>
+                        <div class = "msg-header">
+                            <p>Thanks for visiting DFCAMCLP-IT! Please feel free to leave us a message and let our chatbot help your inquiries with suggested answers.</p>
+                        </div>
                     </div>
-                    <div class = "msg-header">
-                        <p>Thanks for visiting DFCAMCLP-IT! Please feel free to leave us a message and let our chatbot help your inquiries with suggested answers.</p>
-                    </div>
-                </div>
 
-                <div class="user-inbox inbox">
-                    <div class = "msg-header">
-                        <p>Hi, I am a sender</p>
+                    <div class="user-inbox inbox">
+                        <div class = "msg-header">
+                            <p>Hi, I am a sender</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="bot-inbox inbox">   
-                    <div class="icon">
-                        <i class ="user"></i>
+                    <div class="bot-inbox inbox">   
+                        <div class="icon">
+                            <i class ="user"></i>
+                        </div>
+                        <div class = "msg-header">
+                            <p>Hi! How can we help?</p>
+                        </div>
                     </div>
-                    <div class = "msg-header">
-                        <p>Hi! How can we help?</p>
-                    </div>
-                </div>
 
-                <div class="user-inbox inbox">
-                    <div class = "msg-header">
-                        <p>Hi, I am a sender</p>
+                    <div class="user-inbox inbox">
+                        <div class = "msg-header">
+                            <p>Hi, I am a sender</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="type-field">
-                <input type="text" placeholder="Reply here..." required>
-                <button>Send</button>
-            </div>
+                <div class="type-field">
+                    <input type="text" placeholder="Reply here..." required>
+                    <button>Send</button>
+                </div>
         </div>
-    -->
-    <script>
-        var inquirerForm = document.getElementById("inquirerForm");
-        var studentForm = document.getElementById("studentForm");
-
-        var btnStudent = document.getElementById("btnStudent");
-        btnStudent.onclick = function(){
-            inquirerForm.style.left="-320px";
-            studentForm.style.left="20px";
-            
-        }
-    </script>
-        
-
+            -->
     </body>
-    </html>
+</html>
 
 
