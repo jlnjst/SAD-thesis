c1bd81f235fcb7598e6bd939fc1bd9b4db6a382a
xyrelljoi <xyrelljoi@gmail.com>
2021-11-22 21:26:30 +0800

    modify inquirer-connection.php

---
+++ connection/inquirer-connection.php
@@ -0,0 +1,44 @@
+
+
+<?php
+//Get Heroku ClearDB connection information
+function connection(){
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$inquirercon = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+if(!$inquirercon){
+    die("can't reconnect".mysqli_connect_error());
+}
+else{
+    $fname=test_input($_POST['firstname']);
+    $email=test_input($_POST['email']);
+
+    $sql= "INSERT INTO inquirer_list(firstname,email)VALUES('$fname','$email')";
+
+    
+    if(mysqli_query($inquirercon,$sql)){
+        echo "show";
+    }
+    else{
+        echo "fail to insert";
+    }
+    mysqli_close($inquirercon);
+    
+}
+}
+function test_input($logindata){
+    $logindata=trim($logindata);
+    $logindata=stripslashes($logindata);
+    $logindata=htmlspecialchars($logindata);
+    return $logindata;
+}
+
+
+?>
